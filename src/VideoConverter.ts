40ee1f5ac33aab59d56740f9b29fc387e2fce974
Masayuki Ota <masota@microsoft.com>
2017-09-21 10:40:25 -0700

    Update VideoConverter.ts

--- src/VideoConverter.ts
+++ src/VideoConverter.ts
@@ -1,35 +1,33 @@
 import {AbstractConverter} from './AbstractConverter';
 import {Message as DirectLineActivity,VideoCard} from "botframework-directlinejs";
-// import {Client as LineClient} from "@line/bot-sdk"
 
 export class VideoConverter extends AbstractConverter{
     constructor(){super();}
     public lineToDirectLine(event: Line.MessageEvent): DirectLineActivity {
         //Convert Line Video Data to Botframework Video data
-        // var eventMessage: Line.EventMessageBase = event.message;
-        // var moc:DirectLineActivity = new ;
+        //TODO : need to impelment. I need to handle content object to get binary video data https://devdocs.line.me/en/#webhook-event-object
     };
 
     public DirectLineToLine(event: DirectLineActivity): Line.Message{
         console.log("VideoConverter.DirectLineToLine started");
+        let lineVideoMessage: Line.VideoMessage;
         if(event.attachments!=null && event.attachments[0].contentType ==  "application/vnd.microsoft.card.video"){
             let videoCard = event.attachments[0] as VideoCard;
-            let lineVideoMessage: Line.VideoMessage = {
+            lineVideoMessage = {
                 type:'video',
                 originalContentUrl: videoCard.content.media[0].url,
                 previewImageUrl:videoCard.content.image.url
             }
-            console.log("VideoConverter.DirectLineToLine Completed");
-            return lineVideoMessage;
         }
-        //For testing of null data from DirectLine
-        let lineVideoMessage:Line.VideoMessage = {
+        else{
+            //return empty video message
+            lineVideoMessage = {
                 type:'video',
-                // originalContentUrl:'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
-                // previewImageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_big.jpg/220px-Big_buck_bunny_poster_big.jpg'
                 originalContentUrl:'',
                 previewImageUrl:''
+            }
         }
+        console.log("VideoConverter.DirectLineToLine Completed");
         return lineVideoMessage;
     };
 }
